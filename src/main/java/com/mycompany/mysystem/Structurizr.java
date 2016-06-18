9972c785c7838b18b123fc49e908b877de9e98bb
Simon Brown <simon.brown@codingthearchitecture.com>
2016-06-18 11:27:38 +0100

    Some tweaks to make the example more consistent with those on the site.

--- src/main/java/com/mycompany/mysystem/Structurizr.java
+++ src/main/java/com/mycompany/mysystem/Structurizr.java
@@ -2,41 +2,48 @@ package com.mycompany.mysystem;
 
 import com.structurizr.Workspace;
 import com.structurizr.api.StructurizrClient;
-import com.structurizr.io.WorkspaceWriterException;
-import com.structurizr.io.json.JsonWriter;
 import com.structurizr.model.Model;
+import com.structurizr.model.Person;
+import com.structurizr.model.SoftwareSystem;
+import com.structurizr.model.Tags;
+import com.structurizr.view.PaperSize;
 import com.structurizr.view.Styles;
+import com.structurizr.view.SystemContextView;
 import com.structurizr.view.ViewSet;
-
-import java.io.StringWriter;
 
 /**
  * This is a simple example of how to get started with Structurizr for Java.
  */
 public class Structurizr {
 
+    private static final String API_KEY = "key";
+    private static final String API_SECRET = "secret";
+    private static final long WORKSPACE_ID = 1234;
+
     public static void main(String[] args) throws Exception {
-        Workspace workspace = new Workspace("My software system", "A description of my software system.");
+        Workspace workspace = new Workspace("My model", "This is a model of my software system.");
         Model model = workspace.getModel();
         ViewSet viewSet = workspace.getViews();
         Styles styles = viewSet.getConfiguration().getStyles();
 
-        // create your model, views and styles
+        Person user = model.addPerson("User", "A user of my software system.");
+        SoftwareSystem softwareSystem = model.addSoftwareSystem("Software System", "My software system.");
+        user.uses(softwareSystem, "Uses");
 
-        outputWorkspaceToConsole(workspace);
+        SystemContextView contextView = viewSet.createContextView(softwareSystem, "A description of this diagram.");
+        contextView.addAllSoftwareSystems();
+        contextView.addAllPeople();
+        contextView.setPaperSize(PaperSize.A5_Landscape);
+
+        styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
+        styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff");
+
         uploadWorkspaceToStructurizr(workspace);
     }
 
-    private static void outputWorkspaceToConsole(Workspace workspace) throws WorkspaceWriterException {
-        JsonWriter jsonWriter = new JsonWriter(true);
-        StringWriter stringWriter = new StringWriter();
-        jsonWriter.write(workspace, stringWriter);
-        System.out.println(stringWriter.toString());
-    }
-
     private static void uploadWorkspaceToStructurizr(Workspace workspace) throws Exception {
-        StructurizrClient structurizrClient = new StructurizrClient("key", "secret");
-        structurizrClient.putWorkspace(1234, workspace);
+        StructurizrClient structurizrClient = new StructurizrClient(API_KEY, API_SECRET);
+        structurizrClient.mergeWorkspace(WORKSPACE_ID, workspace);
     }
 
 }
