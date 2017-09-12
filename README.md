00e4f7fcc8e0f44d0edc526f516f6849bba65c00
Simon Brown <simon.brown@codingthearchitecture.com>
2017-09-12 14:16:06 +0100

    Some updates to reflect the new version.

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Structurizr for Java starter
 
-This is a simple starting point for using [Structurizr for Java](https://github.com/structurizr/java).
+This is a simple starting point for using [Structurizr for Java](https://github.com/structurizr/java). To use it:
+
+## 1. Clone the repo
+
+```
+git clone https://github.com/structurizr/java-starter.git
+```
+
+## 2. Set your workspace ID, API key and secret
+
+Modify the constants at the top of [java-starter/src/main/java/com/mycompany/mysystem/Structurizr.java](https://github.com/structurizr/java-starter/blob/master/src/main/java/com/mycompany/mysystem/Structurizr.java) to reflect your own Structurizr workspace (this information is available on your dashboard, after signing in).
+
+```java
+private static final String API_KEY = "key";
+private static final String API_SECRET = "secret";
+private static final long WORKSPACE_ID = 1234;
+```
+
+## 3. Run the program
+
+Finally, run the program, using your IDE or Gradle; for example:
+
+```
+cd java-starter
+./gradlew run
+```
+
+For more information, please see [Structurizr for Java - Getting Started](https://github.com/structurizr/java/blob/master/docs/getting-started.md).
--- src/main/java/com/mycompany/mysystem/Structurizr.java
+++ src/main/java/com/mycompany/mysystem/Structurizr.java
@@ -3,15 +3,12 @@ package com.mycompany.mysystem;
 import com.structurizr.Workspace;
 import com.structurizr.api.StructurizrClient;
 import com.structurizr.documentation.Format;
-import com.structurizr.documentation.StructurizrDocumentation;
+import com.structurizr.documentation.StructurizrDocumentationTemplate;
 import com.structurizr.model.Model;
 import com.structurizr.model.Person;
 import com.structurizr.model.SoftwareSystem;
 import com.structurizr.model.Tags;
-import com.structurizr.view.PaperSize;
-import com.structurizr.view.Styles;
-import com.structurizr.view.SystemContextView;
-import com.structurizr.view.ViewSet;
+import com.structurizr.view.*;
 
 /**
  * This is a simple example of how to get started with Structurizr for Java.
@@ -24,10 +21,8 @@ public class Structurizr {
 
     public static void main(String[] args) throws Exception {
         // a Structurizr workspace is the wrapper for a software architecture model, views and documentation
-        Workspace workspace = new Workspace("My model", "This is a model of my software system.");
+        Workspace workspace = new Workspace("Getting Started", "This is a model of my software system.");
         Model model = workspace.getModel();
-        ViewSet viewSet = workspace.getViews();
-        Styles styles = viewSet.getConfiguration().getStyles();
 
         // add some elements to your software architecture model
         Person user = model.addPerson("User", "A user of my software system.");
@@ -35,22 +30,23 @@ public class Structurizr {
         user.uses(softwareSystem, "Uses");
 
         // define some views (the diagrams you would like to see)
-        SystemContextView contextView = viewSet.createSystemContextView(softwareSystem, "Context", "A description of this diagram.");
+        ViewSet views = workspace.getViews();
+        SystemContextView contextView = views.createSystemContextView(softwareSystem, "SystemContext", "An example of a System Context diagram.");
+        contextView.setPaperSize(PaperSize.A5_Landscape);
         contextView.addAllSoftwareSystems();
         contextView.addAllPeople();
-        contextView.setPaperSize(PaperSize.A5_Landscape);
 
         // add some documentation
-        StructurizrDocumentation documentation = new StructurizrDocumentation(model);
-        workspace.setDocumentation(documentation);
-        documentation.addContextSection(softwareSystem, Format.Markdown,
+        StructurizrDocumentationTemplate template = new StructurizrDocumentationTemplate(workspace);
+        template.addContextSection(softwareSystem, Format.Markdown,
                 "Here is some context about the software system...\n" +
                         "\n" +
-                        "![](embed:Context)");
+                        "![](embed:SystemContext)");
 
-        // optionally, add some styling
+        // add some styling
+        Styles styles = views.getConfiguration().getStyles();
         styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
-        styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff");
+        styles.addElementStyle(Tags.PERSON).background("#08427b").color("#ffffff").shape(Shape.Person);
 
         uploadWorkspaceToStructurizr(workspace);
     }
